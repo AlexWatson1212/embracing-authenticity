5a2e0b993baaa1321e8212426c1013f5d4184963
Alex Watson <alexanderclivewatson@gmail.com>
2025-06-01 18:08:25 +0100

    css cjamge and mobile hamburger menu

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.main-nav');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+  });
+});
